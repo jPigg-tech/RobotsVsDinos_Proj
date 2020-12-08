171833bbec6a45899cfa5ce35122b3de39b36882
unknown <jeffpigg5@gmail.com>
2020-12-08 11:30:38 -0600

    Updated Dinos and Robots attack methods

--- RobotsVsDinosaurs/Battlefield.cs
+++ RobotsVsDinosaurs/Battlefield.cs
@@ -9,15 +9,24 @@ namespace RobotsVsDinosaurs
     class Battlefield
     {
         //Member Variables
-        
-
+        public List<Robot> robots;
+        public List<Weapon> weapons;
+        public List<Dinosaur>{Dinosaur sharpTooth }
 
         //Constructor
-        
+
 
 
         //Member Methods 
 
+        public void TheBattle(List<Dinosaur> dinosaurs, Dinosaur health)
+        {
+            dinosaurs.health = 100;
+            while (Dinosaur health > 0)
+            {
 
+            }
+        }
+        // when all of the robots or dinos in the fleet/heard health = 0 the game is over 
     }
 }
