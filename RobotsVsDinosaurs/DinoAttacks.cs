310da839aa2836669f27b856b9e9d4b6f2a09366
unknown <jeffpigg5@gmail.com>
2021-01-08 11:03:03 -0600

    Created a new cutom class DinoAttacks, this class will be used to create custom attacks that the dinosaur will the be able to us in battle. I gave the class two member variables of the attack type and the attack power

---
+++ RobotsVsDinosaurs/DinoAttacks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurs
+{
+    class DinoAttacks
+    {
+        //Member Variables
+        public string attackType;
+        public int attackPower;
+
+        //Constructor
+        public DinoAttacks(string attackType, int attackPower)
+        {
+            this.attackType = attackType;
+            this.attackPower = attackPower;
+        }
+
+
+        //Member Methods 
+    }
+}
